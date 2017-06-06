c321110502394e53f998837d3509f385a200d231
gaoxinbo <gaoxinbo1984@gmail.com>
2017-06-06 21:05:30 +0100

    Introduce Vue and RequireJS

--- app/app.py
+++ app/app.py
@@ -4,23 +4,29 @@ import provider
 
 from flask import Flask
 from flask import render_template
-
+from flask_restful import Resource, Api
 
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 
-
+api = Api(app)
 p = provider.DataProvider()
+
+class getPosition(Resource):
+    def get(self):
+        return p.getCurrentPosition()
+
+api.add_resource(getPosition, '/position')
+
 
 
 
 @app.route("/")
 def index():
-  position = p.getCurrentPosition()
   #portion = p.getPortion()
-  return render_template("index.html", position=position)
+  return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
