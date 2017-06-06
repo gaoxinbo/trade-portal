ff25a9ff0d4f2679a3ac3e65a5fb0577ed44a630
gaoxinbo <gaoxinbo1984@gmail.com>
2017-06-06 11:20:36 +0100

    Add logger

--- task/daily_fetch.py
+++ task/daily_fetch.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import sys
+import logging
+
 import config
 
 import yahoo_finance 
@@ -9,6 +11,22 @@ import pymysql
 def print_err_and_exit():
     print("Usage: password")
     sys.exit(-1)
+
+def get_logger():
+    logger = logging.getLogger("daily_fetch")
+    logger.setLevel(logging.DEBUG)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+
+
+    logger.addHandler(ch)
+
+    return logger
+ 
 
 def get_connection(passwd):
     connection = pymysql.connect(host=config.RDS_ENDPOINT,
@@ -23,17 +41,20 @@ if __name__ == '__main__':
       
 
     conn = get_connection(sys.argv[1])
+    logger = get_logger()
 
     sql = "replace into daily_price (symbol, trade_date, open_price, close_price) values (%s, %s, %s, %s)"
 
     for symbol in config.SYMBOL_LIST:
         with conn.cursor() as cursor:
+            logger.info("fetching " + symbol)
             s = yahoo_finance.Share(symbol)
             opt = [] 
             opt.append(symbol)
-            opt.append(s.get_trade_datetime())
+            opt.append(s.get_trade_datetime()[0:10])
             opt.append(s.get_open())
             opt.append(s.get_price())
+            logger.info(opt)
     
             cursor.execute(sql, opt)
             conn.commit()
