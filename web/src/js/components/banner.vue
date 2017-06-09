2f6ba4a8bcbece949b665c3aa8a167d2335fd591
gaoxinbo <gaoxinbo1984@gmail.com>
2017-06-09 10:50:40 +0100

    Add banner component

--- web/src/js/components/banner.vue
+++ web/src/js/components/banner.vue
@@ -1,16 +1,32 @@
 <template>
-<h1>{{ text }}</h1>
+    <div id="banner">
+        <h1>{{ text }}</h1>
+    </div>
 </template>
 
 <script>
-module.exports = {
-    data : function() {
-        return {
-            text : "hello, this is a banner"
+    module.exports = {
+        data : function() {
+            return {
+                text : "Trading Portal"
+            }
         }
     }
-}
 </script>
 
+<style scoped>
+h1 {
+    color : #FFF;
+    margin-left : 100px;
+};
+
+#banner {
+    background-color:#563d7c;
+    padding-bottom:60px;
+    padding-top:60px;
+    margin-top:20px;
+    margin-bottom:20px;
+}
+</style>
 
 
